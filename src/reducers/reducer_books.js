f4637da1d4f4d700da69e9390d0a6bcc9c1af1ea
Brennan <bmart2913@gmail.com>
2017-11-18 17:36:06 -0500

    initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function (){
+    return [
+        {title: 'JS the good parts', pages:231},
+        {title: 'How to grill',pages:156},
+        {title: 'Eragon, the journey of a boy and his dragon',pages:761},
+        {title: 'the Bible',pages:1045}
+    ];
+}
